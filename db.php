263d66f33d0f8a64bb0cdc917fc0495801362b4f
Renato Cardoso <renatoscardoso77@gmail.com>
2024-10-17 09:25:13 -0400

    update: db.php

--- db.php
+++ db.php
@@ -1,16 +1,22 @@
 <?php
-    //Conexão com db postgress
-    $host = 'aws-0-sa-east-1.pooler.supabase.com';
-    $db = 'postgres';
-    $user = 'postgres.xzdqjbcapveyyomwdsql';
-    $pass = 'x5wRqAKcnIVng12v';
+  // Informações do banco de dados do Supabase
+  $host = 'aws-0-sa-east-1.pooler.supabase.com';
+  $dbname = 'postgres';
+  $username = 'postgres.xzdqjbcapveyyomwdsql';
+  $password = 'YgyKL5Luj071Xgx1';
+  $port = '6543'; // Porta padrão SUPA BASE
 
-    $dsn = 'pgsql:host=$host;dbname=$db';
+  try {
+    // Criar a conexão com o banco de dados usando PDO
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $username, $password);
 
-    try {
-        $pdo = new PDO($dsn, $user, $pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch (PDOException $e) {
-        die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
-    }
+    // Definir o modo de erro para exceções
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Conexão bem-sucedida com o banco de dados do Supabase XÊra meu saco!";
+  } catch (PDOException $e) {
+    // Exibir erro em caso de falha na conexão
+    die("Erro ao conectar: " . $e->getMessage());
+  }
 ?>
